d741beb0e18eece50b4cd15c1e4a0fdc04bba536
Rafael Ponte <rafael.ponte@zup.com.br>
2022-05-10 18:10:15 -0300

    first commit + novo funcionario;

---
+++ src/main/java/br/com/zup/edu/gestao/funcionarios/DetalhesDoFuncionarioResponse.java
@@ -0,0 +1,26 @@
+package br.com.zup.edu.gestao.funcionarios;
+
+public class DetalhesDoFuncionarioResponse {
+
+    private final Long id;
+    private final String nome;
+    private final Cargo cargo;
+
+    public DetalhesDoFuncionarioResponse(Funcionario funcionario) {
+        this.id = funcionario.getId();
+        this.nome = funcionario.getNome();
+        this.cargo = funcionario.getCargo();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+}
